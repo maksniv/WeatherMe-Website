f239022801d1acfd5c56aa8a45b04b7e66374344
maksim <maksniv@gmail.com>
2023-03-14 18:29:46 +0300

    add new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Account from './pages/Account/Account';
+import Home from './pages/Home/Home';
+import Registration from './pages/Registration/Registration';
+import PageNotFound from './pages/PageNotFound/PageNotFound';
 
 function App() {
-  return <h1>Hello React</h1>;
+  return (
+    <>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/account">Account</Link>
+        <Link to="/registration">Registration</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
-
-
-
